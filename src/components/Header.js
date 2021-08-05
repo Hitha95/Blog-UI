dd0a27655d05b93e9ea9b3c0fec9528cdd4815aa
Hitha <86621583+Hitha95@users.noreply.github.com>
2021-08-05 14:00:26 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,5 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { Typography } from '@material-ui/core';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
